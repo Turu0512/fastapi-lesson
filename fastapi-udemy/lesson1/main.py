832997aa98f64d1da47bae986765aabb4f4406e7
Turu0512 <hinataptyan@gmail.com>
2021-12-31 21:05:41 +0900

    POSTメソッド

---
+++ fastapi-udemy/lesson1/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: int
+    tax: Optional[float] = None
+
+app = FastAPI()
+
+@app.post("/item/")
+async def create_item(item: Item):
+	return item
