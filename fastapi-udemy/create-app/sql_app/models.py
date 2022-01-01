6bd62b77118fe0e0cce0eb52d6cdf4004d36191b
Turu0512 <hinataptyan@gmail.com>
2022-01-01 14:22:56 +0900

    SQLite, models.py作成

---
+++ fastapi-udemy/create-app/sql_app/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from .database import Base
+
+
+class Users(Base):
+    __tablename__ = 'users'
+    user_id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+
+class Room(Base):
+    __tablename__ = 'users'
+    room_id = Column(Integer, primary_key=True, index=True)
+    room_name = Column(String, unique=True, index=True)
+    capacity = Column(Integer)
+
+class Bookings(Base):
+    __tablename__ = 'bookings'
+    booking_id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(String, ForeignKey('users.user_id', ondelete='SET NULL'), nullable=False)
+    room_id = Column(String, ForeignKey('rooms.room_id', ondelete='SET NULL'), nullable=False)
+    booked_num = Column(Integer)
+    start_time = Column(DateTime, nullable=False)
+    end_time = Column(DateTime, nullable=False)
