011bd28fee699c8313fb5d5e7895e4b3ee213533
Turu0512 <hinataptyan@gmail.com>
2022-01-01 20:22:01 +0900

    予約画面作成１まで完了

--- fastapi-udemy/create-app/app.py
+++ fastapi-udemy/create-app/app.py
@@ -30,64 +30,80 @@ if page == 'users':
 		st.json(res.json())
 
 elif page == 'rooms':
-	st.title('APTテスト(会議室)')
+	st.title('会議室登録画面')
 	with st.form(key='room'):
-		room_id: int = random.randint(0, 100)
 		room_name: str = st.text_input('客室名', max_chars=12)
 		capacity: int = st.number_input('定員', step=1)
 		data = {
-			'room_id': room_id,
 			'room_name': room_name,
 			'capacity': capacity
 		}
-		submit_button = st.form_submit_button(label='リクエスト送信')
+		submit_button = st.form_submit_button(label='会議室登録')
 
 	if submit_button:
-		st.write('## 送信データ')
-		st.json(data)
-		st.write('## レスポンス結果')
 		url="http://127.0.0.1:8000/rooms"
 		res = requests.post(url, data=json.dumps(data))
-		st.write(res.status_code)
+		if res.status_code ==	200:
+						st.success('会議室登録完了')
 		st.json(res.json())
+		
 
 elif page == 'bookings':
-	st.title('APTテスト(予約)')
-	with st.form(key='booking'):
-		booking_id: int = random.randint(0, 10)
-		user_id: int = random.randint(0, 10)
-		room_id: int = random.randint(0, 10)
-		booked_num: int = st.number_input('予約人数', step=1)
-		date = st.date_input('日付: ',min_value=datetime.date.today())
-		start_time = st.time_input('開始時刻: ',value=datetime.time(hour=9, minute=0))
-		end_time = st.time_input('終了時刻: ',value=datetime.time(hour=20, minute=0))
-		data = {
-			'room_id': room_id,
-			'user_id': user_id,
-			'booking_id': booking_id,
-			'booked_num': booked_num,
-			'start_datetime': datetime.datetime(
-				year=date.year,
-				month=date.month,
-				day=date.day,
-				hour=start_time.hour,
-				minute=start_time.minute
-			).isoformat(),
-			'end_datetime': datetime.datetime(
-				year=date.year,
-				month=date.month,
-				day=date.day,
-				hour=end_time.hour,
-				minute=end_time.minute
-			).isoformat()
-			}	
-		submit_button = st.form_submit_button(label='リクエスト送信')
+	st.title('予約一覧')
+# ユーザー一覧
+url_users = 'http://127.0.0.1:8000/users'
+res = requests.get(url_users)
+users = res.json()
+users_dict = {}
+for user in users:
+				users_dict[user['username']] = user['user_id']
 
-	if submit_button:
-		st.write('## 送信データ')
-		st.json(data)
-		st.write('## レスポンス結果')
-		url="http://127.0.0.1:8000/bookings"
-		res = requests.post(url, data=json.dumps(data))
-		st.write(res.status_code)
-		st.json(res.json())
+# 会議室一覧
+url_rooms = 'http://127.0.0.1:8000/rooms'
+res = requests.get(url_rooms)
+rooms = res.json()
+rooms_dict = {}
+for room in rooms:
+				rooms_dict[room['room_name']] = room['room_id'] = {
+								'room_id': room['room_id'],
+								'capacity': room['capacity']
+				}
+
+	# with st.form(key='booking'):
+	# 	booking_id: int = random.randint(0, 10)
+	# 	user_id: int = random.randint(0, 10)
+	# 	room_id: int = random.randint(0, 10)
+	# 	booked_num: int = st.number_input('予約人数', step=1)
+	# 	date = st.date_input('日付: ',min_value=datetime.date.today())
+	# 	start_time = st.time_input('開始時刻: ',value=datetime.time(hour=9, minute=0))
+	# 	end_time = st.time_input('終了時刻: ',value=datetime.time(hour=20, minute=0))
+	# 	data = {
+	# 		'room_id': room_id,
+	# 		'user_id': user_id,
+	# 		'booking_id': booking_id,
+	# 		'booked_num': booked_num,
+	# 		'start_datetime': datetime.datetime(
+	# 			year=date.year,
+	# 			month=date.month,
+	# 			day=date.day,
+	# 			hour=start_time.hour,
+	# 			minute=start_time.minute
+	# 		).isoformat(),
+	# 		'end_datetime': datetime.datetime(
+	# 			year=date.year,
+	# 			month=date.month,
+	# 			day=date.day,
+	# 			hour=end_time.hour,
+	# 			minute=end_time.minute
+	# 		).isoformat()
+	# 		}	
+	# 	submit_button = st.form_submit_button(label='リクエスト送信')
+
+	# if submit_button:
+	# 	st.write('## 送信データ')
+	# 	st.json(data)
+	# 	st.write('## レスポンス結果')
+	# 	url="http://127.0.0.1:8000/bookings"
+	# 	res = requests.post(url, data=json.dumps(data))
+	# 	st.write(res.status_code)
+	# 	st.json(res.json())
--- fastapi-udemy/create-app/sql_app/models.py
+++ fastapi-udemy/create-app/sql_app/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from .database import Base
 
 
-class Users(Base):
+class User(Base):
     __tablename__ = 'users'
     user_id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
